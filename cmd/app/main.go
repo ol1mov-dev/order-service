eb463007c9f1e1a51c4ce830d71522597142eb5f
manuchehr <manuchehr.dev@gmail.com>
2025-12-10 19:02:56 +0300

    implemented some methods in order_handler and refactored order_db structure

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"order-service/internal/handlers"
 
+	orderv1 "github.com/ol1mov-dev/protos/pkg/order/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	HOST = "localhost"
+	PORT = "8080"
+)
+
+func main() {
+	addr := net.JoinHostPort(HOST, PORT)
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	orderv1.RegisterOrderV1ServiceServer(grpcServer, &handlers.OrderServer{})
+	reflection.Register(grpcServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- migrations/20251205111637_create_orders_table.sql
+++ migrations/20251205111637_create_orders_table.sql
@@ -1,40 +1,32 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE orders (
-    id BIGSERIAL PRIMARY KEY,
-    public_order_number VARCHAR(20) UNIQUE NOT NULL,
-    user_id BIGINT NOT NULL,
+                        id BIGSERIAL PRIMARY KEY,
+                        public_order_number VARCHAR(20) UNIQUE NOT NULL,
+                        user_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     quantity INTEGER NOT NULL CHECK (quantity > 0),
 
-    status VARCHAR(50) NOT NULL DEFAULT 'created' CHECK (
-        status IN (
-                   'created',           -- заказ создан
-                   'pending_payment',   -- ожидает оплаты
-                   'paid',              -- оплачен
-                   'processing',        -- в обработке
-                   'ready_for_shipment',-- готов к отгрузке
-                   'shipped',           -- отправлен
-                   'in_transit',        -- в пути
-                   'out_for_delivery',  -- у курьера
-                   'delivered',         -- доставлен
-                   'cancelled',         -- отменен
-                   'refunded',          -- возвращен
-                   'failed'             -- не удался
-            )
+-- Изменено на INTEGER с маппингом на enum
+    status INTEGER NOT NULL DEFAULT 1 CHECK (
+        status BETWEEN 0 AND 12  -- от ORDER_STATUS_UNSPECIFIED до ORDER_STATUS_FAILED
         ),
     total_amount DECIMAL(15,2) NOT NULL CHECK (total_amount >= 0),
 
-    -- Связь со складом
-    warehouse_id BIGINT,
+-- Связь со складом
+    warehouse_id BIGINT NOT NULL,
 
-    -- Timestamps
+-- Timestamps
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+-- Создаем индекс для статуса если будет много фильтраций по нему
+CREATE INDEX idx_orders_status ON orders(status);
+CREATE INDEX idx_orders_user_id ON orders(user_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS warehouses;
+DROP TABLE IF EXISTS orders;
 -- +goose StatementEnd
